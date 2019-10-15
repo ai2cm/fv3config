99a9e5a0cbf5eac18a4ca619ad13cd71b103a2ca
Jeremy McGibbon <jeremym@vulcan.com>
2019-10-15 16:45:42 -0700

    Added test stubs for ForcingData

---
+++ tests/test_forcingdata.py
@@ -0,0 +1,41 @@
+import unittest
+
+
+class TestForcingData(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_init_from_empty_directory(self):
+        pass
+
+    def test_init_from_one_file_in_directory(self):
+        pass
+
+    def test_init_from_one_file_in_subdirectory(self):
+        pass
+
+    def test_init_from_realistic_directory(self):
+        pass
+
+    def test_link_from_one_file_in_directory(self):
+        pass
+
+    def test_link_from_one_file_in_subdirectory(self):
+        pass
+
+    def test_link_from_realistic_directory(self):
+        pass
+
+    def test_init_from_default_config(self):
+        pass
+
+    def test_link_from_default_config(self):
+        pass
+
+
+if __name__ == '__main__':
+    unitest.main()
