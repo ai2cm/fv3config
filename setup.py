9f4f211a504123d21555531c651bcf918e32990f
Jeremy McGibbon <jeremym@vulcan.com>
2019-10-18 16:01:59 -0700

    Remove python 2 from list of supported languages

--- setup.py
+++ setup.py
@@ -16,6 +16,7 @@ requirements = [
     'f90nml>=1.1.0',
     'appdirs>=1.4.0',
     'wget>=3.0',
+    'gsutil'
 ]
 
 setup_requirements = [ ]
@@ -31,8 +32,6 @@ setup(
         'Intended Audience :: Developers',
         'License :: OSI Approved :: BSD License',
         'Natural Language :: English',
-        "Programming Language :: Python :: 2",
-        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
