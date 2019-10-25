28610261a95c1a95c9557dc7a65050e58722ec37
Jeremy McGibbon <jeremym@vulcan.com>
2019-10-25 14:21:03 -0700

    cleanup python requirements

--- setup.py
+++ setup.py
@@ -26,14 +26,13 @@ test_requirements = [ ]
 setup(
     author="Vulcan Technologies, LLC",
     author_email='jeremym@vulcan.com',
-    python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*',
+    python_requires='>=3.6',
     classifiers=[
         'Development Status :: 2 - Pre-Alpha',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: BSD License',
         'Natural Language :: English',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
     ],
