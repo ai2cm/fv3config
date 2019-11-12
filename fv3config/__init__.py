a316a98bc75f40005fc799c8a2ca4690152d4da0
Jeremy McGibbon <jeremym@vulcan.com>
2019-11-12 15:16:08 -0800

    Moved data refreshing from python api to command-line endpoint

--- fv3config/__init__.py
+++ fv3config/__init__.py
@@ -7,7 +7,7 @@ from ._config import (
     write_run_directory, enable_restart
 )
 from ._exceptions import InvalidFileError, ConfigError
-from ._datastore import refresh_downloaded_data, ensure_data_is_downloaded
+from ._datastore import ensure_data_is_downloaded
 
 __all__ = [
     'config_to_namelist', 'config_from_namelist', 'get_default_config',
