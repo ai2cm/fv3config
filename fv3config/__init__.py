a682ef8a0783559685a78a1ff8b457e98429362d
Jeremy McGibbon <jeremym@vulcan.com>
2019-11-15 14:10:32 -0800

    refactor code to use one global variable for cache location

--- fv3config/__init__.py
+++ fv3config/__init__.py
@@ -9,11 +9,6 @@ from ._config import (
 from ._exceptions import InvalidFileError, ConfigError
 from ._datastore import refresh_downloaded_data, ensure_data_is_downloaded
 
-__all__ = [
-    'config_to_namelist', 'config_from_namelist', 'get_default_config',
-    'write_run_directory',
-    'InvalidFileError', 'ConfigError'
-]
 
 __author__ = """Vulcan Technologies, LLC"""
 __email__ = 'jeremym@vulcan.com'
