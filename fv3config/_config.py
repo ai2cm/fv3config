614b8b74da1b6ac7dae4ea268c7b7b811666178e
Oliver Watt-Meyer <oliwm@vulcan.com>
2019-11-15 15:49:19 -0800

    Use config_filelist in write_run_directory

--- fv3config/_config.py
+++ fv3config/_config.py
@@ -3,14 +3,15 @@ import os
 import f90nml
 from ._exceptions import InvalidFileError, ConfigError
 from ._datastore import (
-    get_base_forcing_directory, get_orographic_forcing_directory,
-    get_initial_conditions_directory, link_or_copy_directory,
-    check_if_data_is_downloaded, copy_file
+    check_if_data_is_downloaded
 )
 from ._tables import (
-    get_field_table_filename, get_diag_table_filename,
-    get_data_table_filename, update_diag_table_for_config,
-    get_current_date_from_config
+    update_diag_table_for_config, get_current_date_from_config
+)
+from ._config_filelist import (
+    get_base_forcing_filelist, get_initial_conditions_filelist, get_orographic_forcing_filelist,
+    get_data_table_filelist_item, get_diag_table_filelist_item, get_field_table_filelist_item,
+    save_filelist_item
 )
 
 package_directory = os.path.dirname(os.path.realpath(__file__))
@@ -99,21 +100,17 @@ def write_run_directory(config, target_directory):
     check_if_data_is_downloaded()
     input_directory = os.path.join(target_directory, 'INPUT')
     restart_directory = os.path.join(target_directory, 'RESTART')
-    initial_conditions_dir = get_initial_conditions_directory(config)
-    base_forcing_dir = get_base_forcing_directory(config)
-    orographic_forcing_dir = get_orographic_forcing_directory(config)
-    field_table_filename = get_field_table_filename(config)
-    diag_table_filename = get_diag_table_filename(config)
-    data_table_filename = get_data_table_filename(config)
+    config_filelist = get_initial_conditions_filelist(config)
+    config_filelist += get_base_forcing_filelist(config)
+    config_filelist += get_orographic_forcing_filelist(config)
+    config_filelist.append(get_field_table_filelist_item(config))
+    config_filelist.append(get_diag_table_filelist_item(config))
+    config_filelist.append(get_data_table_filelist_item(config))
     for directory in [target_directory, input_directory, restart_directory]:
         if not os.path.isdir(directory):
             os.mkdir(directory)
-    link_or_copy_directory(base_forcing_dir, target_directory)
-    link_or_copy_directory(orographic_forcing_dir, input_directory)
-    link_or_copy_directory(initial_conditions_dir, input_directory)
-    copy_file(field_table_filename, os.path.join(target_directory, 'field_table'))
-    copy_file(diag_table_filename, os.path.join(target_directory, 'diag_table'))
-    copy_file(data_table_filename, os.path.join(target_directory, 'data_table'))
+    for filelist_item in config_filelist:
+        save_filelist_item(filelist_item)
     current_date = get_current_date_from_config(config, input_directory)
     update_diag_table_for_config(config, current_date, os.path.join(target_directory, 'diag_table'))
     config_to_namelist(config, os.path.join(target_directory, 'input.nml'))
