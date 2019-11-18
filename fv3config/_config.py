9223983a9daaf18e10015cfe35d198d2b288cc9e
Oliver Watt-Meyer <oliwm@vulcan.com>
2019-11-18 12:35:39 -0800

    Fix directory arg in write_asest_list call

--- fv3config/_config.py
+++ fv3config/_config.py
@@ -96,7 +96,7 @@ def write_run_directory(config, target_directory):
     for directory in [target_directory, input_directory, restart_directory]:
         if not os.path.isdir(directory):
             os.mkdir(directory)
-    write_asset_list(asset_list, input_directory)
+    write_asset_list(asset_list, target_directory)
     current_date = get_current_date_from_config(config, input_directory)
     update_diag_table_for_config(config, current_date, os.path.join(target_directory, 'diag_table'))
     config_to_namelist(config, os.path.join(target_directory, 'input.nml'))
