4ddddd6484677f53a49116d32c808e86266e0d19
Oliver Watt-Meyer <oliwm@vulcan.com>
2019-11-19 16:07:14 -0800

    Add __all__ back to __init__ :)

--- fv3config/__init__.py
+++ fv3config/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-"""Top-level package for FV3Config."""
+"""Top-level package for fv3config."""
 
 from ._config import (
     config_to_namelist, config_from_namelist, get_default_config,
@@ -10,8 +10,11 @@ from ._exceptions import InvalidFileError, ConfigError
 from ._datastore import (
     ensure_data_is_downloaded, set_cache_dir, get_cache_dir
 )
+from ._asset_list import generate_asset
 
 
 __author__ = """Vulcan Technologies LLC"""
 __email__ = 'jeremym@vulcan.com'
 __version__ = '0.1.0'
+
+__all__ = list(locals().keys())
