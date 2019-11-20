20a4b1df97377c5b47d0aa5cf961929853cd1ba5
Oliver Watt-Meyer <oliwm@vulcan.com>
2019-11-19 18:21:05 -0800

    Add doc strings for asset_list

--- fv3config/_asset_list.py
+++ fv3config/_asset_list.py
@@ -17,6 +17,7 @@ def is_dict_or_list(option):
 
 
 def ensure_is_list(asset):
+    """If argument is a dict, return length 1 list containing dict"""
     if isinstance(asset, dict):
         return [asset]
     elif isinstance(asset, list):
@@ -26,11 +27,13 @@ def ensure_is_list(asset):
 
 
 def get_orographic_forcing_asset_list(config):
+    """Return asset_list for orographic forcing"""
     source_directory = get_orographic_forcing_directory(config)
     return asset_list_from_path(source_directory, 'INPUT', copy_method='link')
 
 
 def get_base_forcing_asset_list(config):
+    """Return asset_list for base forcing"""
     if is_dict_or_list(config['forcing']):
         return ensure_is_list(config['forcing'])
     else:
@@ -39,6 +42,7 @@ def get_base_forcing_asset_list(config):
 
 
 def get_initial_conditions_asset_list(config):
+    """Return asset_list for initial conditions. """
     if is_dict_or_list(config['initial_conditions']):
         return ensure_is_list(config['initial_conditions'])
     else:
@@ -47,18 +51,21 @@ def get_initial_conditions_asset_list(config):
 
 
 def get_data_table_asset(config):
+    """Return asset for data_table"""
     data_table_filename = get_data_table_filename(config)
     location, name = os.path.split(data_table_filename)
     return generate_asset(location, name, target_name='data_table')
 
 
 def get_diag_table_asset(config):
+    """Return asset for diag_table"""
     diag_table_filename = get_diag_table_filename(config)
     location, name = os.path.split(diag_table_filename)
     return generate_asset(location, name, target_name='diag_table')
 
 
 def get_field_table_asset(config):
+    """Return asset for field_table"""
     field_table_filename = get_field_table_filename(config)
     location, name = os.path.split(field_table_filename)
     return generate_asset(location, name, target_name='field_table')
@@ -66,6 +73,19 @@ def get_field_table_asset(config):
 
 def generate_asset(source_location, source_name, target_location='',
                    target_name=None, copy_method='copy'):
+    """Helper function to generate asset for a particular file
+
+    Args:
+        source_location (str): path to directory containing source file
+        source_name (str): filename of source file
+        target_location (str, optional): sub-directory to which file will
+            be written, relative to run directory root. Defaults to empty
+            string (i.e. root of run directory).
+        target_name (str, optional): filename to which file will be written.
+            Defaults to None, in which case source_name is used.
+        copy_method (str, optional): flag to determine whether file is
+            copied ('copy') or hard-linked ('link'). Defaults to 'copy'.
+    """
     if target_name is None:
         target_name = source_name
     asset = {
@@ -79,6 +99,7 @@ def generate_asset(source_location, source_name, target_location='',
 
 
 def asset_list_from_path(source_directory, target_directory='', copy_method='copy'):
+    """Return an asset_list given either a local path or remote google storage url"""
     if is_gsbucket_url(source_directory):
         return asset_list_from_gs_bucket(source_directory, target_directory)
     else:
@@ -87,8 +108,10 @@ def asset_list_from_path(source_directory, target_directory='', copy_method='cop
 
 
 def asset_list_from_local_dir(source_directory, target_directory='', copy_method='copy'):
-    """Return asset_list from all files in source_directory with target location equal to
-    target_directory. Will recurse to subdirectories if they exist."""
+    """Return asset_list from all files in source_directory (which is assumed to be
+    local) with target location equal to target_directory. Will recurse to
+    subdirectories if they exist.
+    """
     asset_list = []
     for base_filename in os.listdir(source_directory):
         source_item = os.path.join(source_directory, base_filename)
@@ -106,12 +129,18 @@ def asset_list_from_local_dir(source_directory, target_directory='', copy_method
 
 
 def asset_list_from_gs_bucket(source_directory, target_directory='/'):
-    """Return asset_list from all files in source_directory with target location equal to
-    target_directory"""
+    """Return asset_list from all files in source_directory (which is assumed to be
+    a google cloud storage url) with target location equal to target_directory"""
     #TODO: return asset_list given google storage bucket path
 
 
 def write_asset(asset, target_directory):
+    """Write file represented by asset to target_directory
+
+    Args:
+        asset (dict): an asset dict
+        target_directory (str): path to a directory in which all files will be written
+    """
     check_asset_valid(asset)
     source_path = os.path.join(asset['source_location'], asset['source_name'])
     target_path = os.path.join(target_directory, asset['target_location'], asset['target_name'])
@@ -129,11 +158,13 @@ def write_asset(asset, target_directory):
 
 
 def write_asset_list(asset_list, target_directory):
+    """Loop over list of assets and write them all"""
     for asset in asset_list:
         write_asset(asset, target_directory)
 
 
 def check_asset_valid(asset):
+    """Check asset has all of its required keys"""
     required_asset_properties = ['source_location', 'source_name', 'target_location',
                                  'target_name', 'copy_method']
     for required_asset_property in required_asset_properties:
@@ -142,6 +173,8 @@ def check_asset_valid(asset):
 
 
 def config_to_asset_list(config):
+    """Convert a configuration dictionary to an asset list. The asset list
+    will contain all files for the run directory except the namelist."""
     asset_list = get_initial_conditions_asset_list(config)
     asset_list += get_base_forcing_asset_list(config)
     asset_list += get_orographic_forcing_asset_list(config)
