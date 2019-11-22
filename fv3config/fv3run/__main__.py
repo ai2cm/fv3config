7b8b4ca13741d65455f217802f4c2483feaa8f51
Jeremy McGibbon <jeremym@vulcan.com>
2019-11-22 09:24:45 -0800

    clean up old comments

--- fv3config/fv3run/__main__.py
+++ fv3config/fv3run/__main__.py
@@ -3,8 +3,6 @@ import argparse
 from ._run import run
 
 MODULE_NAME = 'fv3config.run'
-# RESTART_DIR_PATTERN = "gs://vcm-ml-data/2019-10-28-X-SHiELD-2019-10-05-multiresolution-extracted/restart/C48/{time}/rundir"
-# OUTPUT_DIR_PATTERN = "gs://vcm-ml-data/2019-10-28-X-SHiELD-2019-10-05-multiresolution-extracted/one-step-run/C48/{time}/rundir"
 STDOUT_FILENAME = 'stdout.log'
 STDERR_FILENAME = 'stderr.log'
 DOCKER_FLAGS = '-it'
@@ -26,13 +24,8 @@ Will use google cloud storage key at $GOOGLE_APPLICATION_CREDENTIALS by default.
 
 
 def main():
-    """Run the FV3GFS model one step forward for the run-directory for a specific time
-
-    Args:
-        time: the timestep YYYYMMDD.HHMMSS to run the model for
-        rundir_transformations: a sequence of transformations (e.g. modifying the diag_table) to
-           apply to the downloaded run directory before the FV3 simulation.
-
+    """Run the FV3GFS model based on a configuration dictionary.
+    Copies the resulting run directory to a target location.
     """
     args = _parse_args()
     run(args.config, args.outdir, args.runfile, args.dockerimage, args.keyfile)
