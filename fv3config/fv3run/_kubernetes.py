7547fc68c9d88ca7ad33e0ae25e561849b435dc5
Andre Perkins <frodre@gmail.com>
2020-01-10 22:29:06 +0000

    linting

--- fv3config/fv3run/_kubernetes.py
+++ fv3config/fv3run/_kubernetes.py
@@ -140,15 +140,13 @@ def _get_kube_command(config_location, outdir, runfile=None):
 def _container_to_job(container, kube_config):
     # Toleration allows operation on the bigger nodes (with the specified taint)
     toleration = kube.client.V1Toleration(
-       effect="NoSchedule",
-       key="dedicated",
-       value="climate-sim-pool",
+       effect="NoSchedule", key="dedicated", value="climate-sim-pool",
     )
     pod_spec = kube.client.V1PodSpec(
         restart_policy="Never",
         containers=[container],
         volumes=kube_config.volumes,
-        tolerations=[toleration]
+        tolerations=[toleration],
     )
     template_spec = kube.client.V1PodTemplateSpec(
         metadata=kube.client.V1ObjectMeta(labels={"app": "fv3run"}), spec=pod_spec,
