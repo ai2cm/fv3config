51a830222b3f312fdc7f635de35844a9c1ccf087
Oliver Watt-Meyer <oliwm@vulcan.com>
2020-01-28 13:00:55 -0800

    Fix test_fv3run for oversubscribed testing system (#55)
    * Fix test_fv3run_with_mocked... for --oversubcribe
    
    * Lint
    
    * Actually fix test
    
    * allow removing multiple oversubscribe flags
    
    Co-authored-by: Jeremy McGibbon <jeremy.mcgibbon@gmail.com>

--- tests/test_fv3run.py
+++ tests/test_fv3run.py
@@ -113,7 +113,11 @@ def test_fv3run_with_mocked_subprocess(runner):
     ):
         runner(fv3config.get_default_config(), outdir)
         assert mock.called
-        assert mock.call_args[0] == (
+        call_args = list(mock.call_args[0])
+        # ensure test does not depend on # of processors on testing system
+        while "--oversubscribe" in call_args[0]:
+            call_args[0].remove("--oversubscribe")
+        assert call_args == [
             [
                 "mpirun",
                 "-n",
@@ -125,7 +129,7 @@ def test_fv3run_with_mocked_subprocess(runner):
                 "mpi4py",
                 "mock_runscript.py",
             ],
-        )
+        ]
         config = yaml.safe_load(open(os.path.join(outdir, "fv3config.yml"), "r"))
         assert config == fv3config.get_default_config()
 
