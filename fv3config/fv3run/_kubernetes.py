fd04e9427efe7ec16aec6bff5e867795c2c3b97b
Noah D. Brenowitz <nbren12@gmail.com>
2020-03-18 22:24:06 +0000

    Adapt fv3config for one-step workflow
    * do not check that output is remote in run_kubernetes
    * do not pipe stderr/out to file in run_native

--- fv3config/fv3run/_kubernetes.py
+++ fv3config/fv3run/_kubernetes.py
@@ -89,7 +89,7 @@ def _get_job(
     image_pull_policy="IfNotPresent",
     job_labels=None,
 ):
-    _ensure_locations_are_remote(config_location, outdir)
+    # _ensure_locations_are_remote(config_location, outdir)
     kube_config = KubernetesConfig(
         jobname,
         memory_gb,
@@ -142,7 +142,7 @@ def _create_job_object(config_location, outdir, docker_image, runfile, kube_conf
 
 def _get_name_from_image(docker_image):
     name = os.path.basename(docker_image)
-    return re.split(r"\W+", name)[0]
+    return re.split(r"\W+", name)[0].replace("_", "-")
 
 
 def _get_kube_command(config_location, outdir, runfile=None):
