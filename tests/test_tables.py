ai2cm/fv3config

commit 0348a71f1cac9babe82cf23ae5a10c4345ebe14f

Add docker file, update enable_restart, add nudging module (#104)
This PR implements a few changes that are necessary in order to allow segmented fv3gfs runs that include nudging to GFS analysis:

- Add a Dockerfile to produce a lightweight image with fv3config installed
- Update enable_restart function to take a required initial_conditions argument and fix a bug in which the function didn't set force_date_from_namelist to False (for a restart run, want to read start date from coupler.res file).
- Add new public API functions get_nudging_assets and update_config_for_nudging.
- Add CLI entry points for enable_restart and update_config_for_nudging.

In order to allow update_config_for_nudging to have only the config object as an argument the get_current_date function is refactored to not require the path to the INPUT directory.

fv3config/config/derive.py
@@ -1,8 +1,10 @@
 import os
 import re
 from datetime import timedelta
+import fsspec
 from .._exceptions import ConfigError
 from .default import NAMELIST_DEFAULTS
+from ..filesystem import get_fs
 
 
 def get_n_processes(config):
@@ -40,15 +42,12 @@ def get_run_duration(config):
     )
 
 
-def get_current_date(config, input_directory):
-    """Return current_date from configuration dictionary and the local INPUT
-    directory. The INPUT directory is passed in order to read current_date from
-    any initial conditions files, since config can contain a remote data source
-    and we do not want to download data in this function.
+def get_current_date(config):
+    """Return current_date from configuration dictionary. This function may read from
+    the remote initial_conditions path in the given configuration dictionary.
 
     Args:
         config (dict): a configuration dictionary
-        input_directory (str): path to local INPUT directory
 
     Returns:
         list: current_date as list of ints [year, month, day, hour, min, sec]
@@ -62,8 +61,8 @@ def get_current_date(config, input_directory):
             "current_date", [0, 0, 0, 0, 0, 0]
         )
     else:
-        coupler_res_filename = os.path.join(input_directory, "coupler.res")
-        if os.path.exists(coupler_res_filename):
+        coupler_res_filename = os.path.join(config["initial_conditions"], "coupler.res")
+        if get_fs(coupler_res_filename).exists(coupler_res_filename):
             current_date = _get_current_date_from_coupler_res(coupler_res_filename)
         else:
             current_date = config["namelist"]["coupler_nml"].get(
@@ -81,7 +80,7 @@ def _get_current_date_from_coupler_res(coupler_res_filename):
     Returns:
         list: current_date as list of ints [year, month, day, hour, min, sec]
     """
-    with open(coupler_res_filename) as f:
+    with fsspec.open(coupler_res_filename, mode="r") as f:
         third_line = f.readlines()[2]
         current_date = [int(d) for d in re.findall(r"\d+", third_line)]
         if len(current_date) != 6:

tests/test_nudging.py
@@ -1,0 +1,172 @@
+import pytest
+from datetime import datetime, timedelta
+
+from fv3config.config import nudging
+import fv3config
+
+
+@pytest.mark.parametrize(
+    "start_time, interval, expected",
+    [
+        (datetime(2016, 1, 1), timedelta(hours=6), datetime(2016, 1, 1, 0)),
+        (datetime(2016, 1, 1, 1), timedelta(hours=6), datetime(2016, 1, 1, 0)),
+        (datetime(2016, 1, 1, 1), timedelta(hours=2), datetime(2016, 1, 1, 0)),
+        (datetime(2016, 1, 1, 3), timedelta(hours=2), datetime(2016, 1, 1, 2)),
+        (datetime(2016, 1, 1, 7), timedelta(hours=6), datetime(2016, 1, 1, 6)),
+        (datetime(2016, 1, 1, 12), timedelta(hours=6), datetime(2016, 1, 1, 12)),
+        (datetime(2016, 1, 2, 18, 1), timedelta(hours=6), datetime(2016, 1, 2, 18)),
+    ],
+)
+def test__get_first_nudge_file_time(start_time, interval, expected):
+    assert nudging._most_recent_nudge_time(start_time, interval) == expected
+
+
+@pytest.mark.parametrize(
+    "duration, current_date, interval, expected_length, "
+    "expected_first_datetime, expected_last_datetime",
+    [
+        (
+            timedelta(days=1),
+            [2016, 1, 1, 0, 0, 0],
+            timedelta(hours=6),
+            4 + 1,
+            datetime(2016, 1, 1),
+            datetime(2016, 1, 2),
+        ),
+        (
+            timedelta(days=1),
+            [2016, 1, 1, 0, 0, 0],
+            timedelta(hours=2),
+            12 + 1,
+            datetime(2016, 1, 1),
+            datetime(2016, 1, 2),
+        ),
+        (
+            timedelta(days=1, hours=5),
+            [2016, 1, 1, 0, 0, 0],
+            timedelta(hours=6),
+            4 + 1 + 1,
+            datetime(2016, 1, 1),
+            datetime(2016, 1, 2, 6),
+        ),
+        (
+            timedelta(days=1, hours=7),
+            [2016, 1, 1, 0, 0, 0],
+            timedelta(hours=6),
+            4 + 2 + 1,
+            datetime(2016, 1, 1),
+            datetime(2016, 1, 2, 12),
+        ),
+        (
+            timedelta(days=1),
+            [2016, 1, 2, 1, 0, 0],
+            timedelta(hours=6),
+            4 + 2,
+            datetime(2016, 1, 2),
+            datetime(2016, 1, 3, 6),
+        ),
+    ],
+)
+def test__get_nudge_time_list(
+    duration,
+    current_date,
+    interval,
+    expected_length,
+    expected_first_datetime,
+    expected_last_datetime,
+):
+    nudge_file_list = nudging._get_nudge_time_list(duration, current_date, interval)
+    assert len(nudge_file_list) == expected_length
+    assert nudge_file_list[0] == expected_first_datetime
+    assert nudge_file_list[-1] == expected_last_datetime
+
+
+def test_get_nudging_assets():
+    nudge_url = "/path/to/files"
+    pattern = "%Y%m%d_%H.nc"
+    assets = fv3config.get_nudging_assets(
+        timedelta(hours=6),
+        [2016, 1, 1, 0, 0, 0],
+        nudge_url,
+        nudge_filename_pattern=pattern,
+    )
+    expected = [
+        fv3config.get_asset_dict(nudge_url, "20160101_00.nc", target_location="INPUT"),
+        fv3config.get_asset_dict(nudge_url, "20160101_06.nc", target_location="INPUT"),
+    ]
+    assert assets == expected
+
+
+def test_get_nudging_assets_raises_config_error_if_linking_remote_files():
+    with pytest.raises(fv3config.ConfigError):
+        fv3config.get_nudging_assets(
+            timedelta(hours=6),
+            [2016, 1, 1, 0, 0, 0],
+            "gs://bucket/path",
+            copy_method="link",
+        )
+
+
+@pytest.mark.parametrize(
+    "nudging_assets, other_items, pattern",
+    [
+        ([], [], "%Y%m%d_%H.nc"),
+        ([fv3config.get_asset_dict("/path", "20160101_00.nc")], [], "%Y%m%d_%H.nc"),
+        ([], ["non asset item"], "%Y%m%d_%H.nc"),
+        ([], [{"other": "non asset item"}], "%Y%m%d_%H.nc"),
+        (
+            [fv3config.get_asset_dict("/path", "20160101_00.nc")],
+            ["non asset item"],
+            "%Y%m%d_%H.nc",
+        ),
+        (
+            [fv3config.get_asset_dict("/path", "20160101_00.nc")],
+            [fv3config.get_asset_dict("/path", "non nudging asset"), "non asset"],
+            "%Y%m%d_%H.nc",
+        ),
+    ],
+)
+def test__non_nudging_assets(nudging_assets, other_items, pattern):
+    input_assets = nudging_assets + other_items
+    cleared_assets = nudging._non_nudging_assets(input_assets, pattern)
+    for item in nudging_assets:
+        assert item not in cleared_assets
+    for item in other_items:
+        assert item in cleared_assets
+
+
+@pytest.mark.parametrize(
+    "item, pattern, expected",
+    [
+        (fv3config.get_asset_dict("/path", "20160101_00.nc"), "%Y%m%d_%H.nc", True),
+        (fv3config.get_asset_dict("/path", "not_nudging_file"), "%Y%m%d_%H.nc", False),
+    ],
+)
+def test__is_nudging_asset(item, pattern, expected):
+    assert nudging._is_nudging_asset(item, pattern) == expected
+
+
+def test_update_config_for_nudging():
+    url = "/path/to/nudging/files"
+    pattern = "%Y%m%d_%H.nc"
+    old_nudging_file = "20151231_18.nc"
+    new_nudging_file = "20160101_06.nc"
+    old_asset = fv3config.get_asset_dict(url, old_nudging_file, target_location="INPUT")
+    new_asset = fv3config.get_asset_dict(url, new_nudging_file, target_location="INPUT")
+    test_config = {
+        "gfs_analysis_data": {"url": url, "filename_pattern": pattern},
+        "initial_conditions": "/path/to/initial_conditions",
+        "namelist": {
+            "coupler_nml": {"current_date": [2016, 1, 1, 0, 0, 0], "hours": 12},
+            "fv_nwp_nudge_nml": {"file_names": [f"INPUT/{old_nudging_file}"]},
+        },
+        "patch_files": [old_asset],
+    }
+
+    fv3config.update_config_for_nudging(test_config)
+
+    updated_file_names = test_config["namelist"]["fv_nwp_nudge_nml"]["file_names"]
+    assert f"INPUT/{old_nudging_file}" not in updated_file_names
+    assert f"INPUT/{new_nudging_file}" in updated_file_names
+    assert old_asset not in test_config["patch_files"]
+    assert new_asset in test_config["patch_files"]

tests/test_tables.py
@@ -162,22 +162,18 @@ class TableTests(unittest.TestCase):
             _get_current_date_from_coupler_res(coupler_res_filename)
 
     def test_get_current_date_from_config(self):
-        rundir = self.make_run_directory("test_rundir")
         config = DEFAULT_CONFIG.copy()
         config["namelist"]["coupler_nml"]["force_date_from_namelist"] = True
         config["namelist"]["coupler_nml"]["current_date"] = valid_current_date
-        current_date = get_current_date(config, os.path.join(rundir, "INPUT"))
+        current_date = get_current_date(config)
         self.assertEqual(current_date, valid_current_date)
 
     def test_update_diag_table_for_config(self):
         rundir = self.make_run_directory("test_rundir")
-        input_directory = os.path.join(rundir, "INPUT")
         diag_table_filename = os.path.join(rundir, "diag_table")
         with open(diag_table_filename, "w") as f:
             f.write(diag_table_test_in)
-        current_date = get_current_date(
-            config_for_update_diag_table_test, input_directory
-        )
+        current_date = get_current_date(config_for_update_diag_table_test)
         update_diag_table_for_config(
             config_for_update_diag_table_test, current_date, diag_table_filename
         )

Dockerfile
@@ -1,0 +1,7 @@
+FROM python:3.7.8-slim-stretch
+
+COPY requirements.txt /tmp/requirements.txt
+RUN pip3 install -r /tmp/requirements.txt && rm -f /tmp/requirements.txt
+
+COPY . /fv3config
+RUN cd /fv3config && pip3 install --no-deps --no-cache-dir . && rm -rf /fv3config

fv3config/__init__.py
@@ -12,6 +12,8 @@ from .config import (
     set_run_duration,
     get_timestep,
     config_from_yaml,
+    get_nudging_assets,
+    update_config_for_nudging,
 )
 from ._exceptions import InvalidFileError, ConfigError
 from ._datastore import ensure_data_is_downloaded

fv3config/cli.py
@@ -6,7 +6,7 @@ import fsspec
 import fv3config
 
 
-def _parse_args():
+def _parse_write_run_directory_args():
     parser = argparse.ArgumentParser("write_run_directory")
     parser.add_argument(
         "config", help="URI to fv3config yaml file. Supports any path used by fsspec."
@@ -17,10 +17,59 @@ def _parse_args():
     return parser.parse_args()
 
 
+def _parse_enable_restart_args():
+    parser = argparse.ArgumentParser("enable_restart")
+    parser.add_argument(
+        "config",
+        help="URI to fv3config yaml file. Supports any path used by fsspec. "
+        "File will be modified in place.",
+    )
+    parser.add_argument(
+        "initial_conditions", help="Path to restart initial conditions.",
+    )
+    return parser.parse_args()
+
+
+def _parse_update_config_for_nudging_args():
+    parser = argparse.ArgumentParser("update_config_for_nudging")
+    parser.add_argument(
+        "config",
+        help="URI to fv3config yaml file. Supports any path used by fsspec. "
+        "File will be modified in place.",
+    )
+    return parser.parse_args()
+
+
 def write_run_directory():
-    args = _parse_args()
+    args = _parse_write_run_directory_args()
 
     with fsspec.open(args.config) as f:
         config = yaml.safe_load(f)
 
     fv3config.write_run_directory(config, args.rundir)
+
+
+def enable_restart():
+    args = _parse_enable_restart_args()
+
+    with fsspec.open(args.config) as f:
+        config = yaml.safe_load(f)
+
+    restart_config = fv3config.enable_restart(config, args.initial_conditions)
+
+    with fsspec.open(args.config, mode="w") as f:
+        yaml.safe_dump(restart_config, f)
+
+
+def update_config_for_nudging():
+    args = _parse_update_config_for_nudging_args()
+
+    with fsspec.open(args.config) as f:
+        config = yaml.safe_load(f)
+
+    # only update config if nudging is turned on
+    if config["namelist"]["fv_core_nml"].get("nudge", False):
+        fv3config.update_config_for_nudging(config)
+
+        with fsspec.open(args.config, mode="w") as f:
+            yaml.safe_dump(config, f)

fv3config/config/__init__.py
@@ -7,6 +7,7 @@ from .namelist import (
 from .rundir import write_run_directory
 from .alter import enable_restart, set_run_duration
 from .derive import get_n_processes, get_run_duration, get_timestep
+from .nudging import get_nudging_assets, update_config_for_nudging
 
 
 def get_default_config():

fv3config/config/alter.py
@@ -4,26 +4,29 @@ from .time_constants import SECONDS_IN_DAY
 from .._exceptions import ConfigError
 
 
-def enable_restart(config):
+def enable_restart(config, initial_conditions):
     """Apply namelist settings for initializing from model restart files.
 
     Args:
         config (dict): a configuration dictionary
+        initial_conditions (str): path to desired new initial conditions.
 
     Returns:
         dict: a configuration dictionary
     """
     if "namelist" not in config:
         raise ConfigError("config dictionary must have a 'namelist' key")
-    if "fv_core_nml" not in config["namelist"]:
-        raise ConfigError("config dictionary must have a 'fv_core_nml' namelist")
     restart_config = copy.deepcopy(config)
-    restart_config["namelist"]["fv_core_nml"]["external_ic"] = False
+    restart_config["namelist"].setdefault("fv_core_nml", {})["external_ic"] = False
     restart_config["namelist"]["fv_core_nml"]["nggps_ic"] = False
     restart_config["namelist"]["fv_core_nml"]["make_nh"] = False
     restart_config["namelist"]["fv_core_nml"]["mountain"] = True
     restart_config["namelist"]["fv_core_nml"]["warm_start"] = True
     restart_config["namelist"]["fv_core_nml"]["na_init"] = 0
+    restart_config["namelist"].setdefault("coupler_nml", {})[
+        "force_date_from_namelist"
+    ] = False
+    restart_config["initial_conditions"] = initial_conditions
     return restart_config
 
 

commit a0799928824a4f10617cf4615e8837c81ccb1f2d

Check for coupler.res among all assets (#107)
Check for the existence of coupler.res amongst all assets of given configuration dictionary. This fixes a bug introduced in #104 which required initial_conditions to be a path, whereas it can also be an asset or list of assets.

As part of this:
- move get_resolution from derive.py to _datastore.py (this is the only module in which this function is used)
- add two tests of get_current_date which exercise the two code paths of that function which are not currently tested

fv3config/_datastore.py
@@ -1,7 +1,6 @@
 import os
 
 from .caching import get_internal_cache_dir
-from .config.derive import get_resolution
 from .data import DATA_DIR
 from ._exceptions import ConfigError
 from . import filesystem
@@ -20,6 +19,28 @@ FIELD_TABLE_OPTIONS = {
     "GFDLMP": "field_table_GFDLMP",
     "ZhaoCarr": "field_table_ZhaoCarr",
 }
+
+
+def get_resolution(config):
+    """Get the model resolution based on a configuration dictionary.
+
+    Args:
+        config (dict): a configuration dictionary
+
+    Returns:
+        resolution (str): a model resolution (e.g. 'C48' or 'C96')
+
+    Raises:
+        ConfigError: if the number of processors in x and y on a tile are unequal
+    """
+    npx = config["namelist"]["fv_core_nml"]["npx"]
+    npy = config["namelist"]["fv_core_nml"]["npy"]
+    if npx != npy:
+        raise ConfigError(
+            f"npx and npy in fv_core_nml must be equal, but are {npx} and {npy}"
+        )
+    resolution = f"C{npx-1}"
+    return resolution
 
 
 def get_orographic_forcing_directory(config):

fv3config/config/derive.py
@@ -4,7 +4,7 @@ from datetime import timedelta
 import fsspec
 from .._exceptions import ConfigError
 from .default import NAMELIST_DEFAULTS
-from ..filesystem import get_fs
+from .._asset_list import config_to_asset_list
 
 
 def get_n_processes(config):
@@ -61,8 +61,8 @@ def get_current_date(config):
             "current_date", [0, 0, 0, 0, 0, 0]
         )
     else:
-        coupler_res_filename = os.path.join(config["initial_conditions"], "coupler.res")
-        if get_fs(coupler_res_filename).exists(coupler_res_filename):
+        coupler_res_filename = _get_coupler_res_filename(config)
+        if coupler_res_filename is not None:
             current_date = _get_current_date_from_coupler_res(coupler_res_filename)
         else:
             current_date = config["namelist"]["coupler_nml"].get(
@@ -90,26 +90,19 @@ def _get_current_date_from_coupler_res(coupler_res_filename):
     return current_date
 
 
-def get_resolution(config):
-    """Get the model resolution based on a configuration dictionary.
-
-    Args:
-        config (dict): a configuration dictionary
-
-    Returns:
-        resolution (str): a model resolution (e.g. 'C48' or 'C96')
-
-    Raises:
-        ConfigError: if the number of processors in x and y on a tile are unequal
-    """
-    npx = config["namelist"]["fv_core_nml"]["npx"]
-    npy = config["namelist"]["fv_core_nml"]["npy"]
-    if npx != npy:
-        raise ConfigError(
-            f"npx and npy in fv_core_nml must be equal, but are {npx} and {npy}"
-        )
-    resolution = f"C{npx-1}"
-    return resolution
+def _get_coupler_res_filename(config):
+    """Return source path for coupler.res file, if it exists in config assets."""
+    asset_list = config_to_asset_list(config)
+    source_path = None
+    for item in asset_list:
+        if item["target_name"] == "coupler.res" and item["target_location"] == "INPUT":
+            if "bytes" in item:
+                raise NotImplementedError(
+                    "Using a bytes dict to represent a coupler.res file is not "
+                    "implemented yet. Use a standard asset dict for this item."
+                )
+            source_path = os.path.join(item["source_location"], item["source_name"])
+    return source_path
 
 
 def get_timestep(config):

tests/test_nudging.py
@@ -5,6 +5,11 @@ from fv3config.config import nudging
 import fv3config
 
 
+@pytest.fixture
+def test_config(c12_config):
+    return c12_config
+
+
 @pytest.mark.parametrize(
     "start_time, interval, expected",
     [
@@ -146,22 +151,20 @@ def test__is_nudging_asset(item, pattern, expected):
     assert nudging._is_nudging_asset(item, pattern) == expected
 
 
-def test_update_config_for_nudging():
+def test_update_config_for_nudging(test_config):
     url = "/path/to/nudging/files"
     pattern = "%Y%m%d_%H.nc"
     old_nudging_file = "20151231_18.nc"
     new_nudging_file = "20160101_06.nc"
     old_asset = fv3config.get_asset_dict(url, old_nudging_file, target_location="INPUT")
     new_asset = fv3config.get_asset_dict(url, new_nudging_file, target_location="INPUT")
-    test_config = {
-        "gfs_analysis_data": {"url": url, "filename_pattern": pattern},
-        "initial_conditions": "/path/to/initial_conditions",
-        "namelist": {
-            "coupler_nml": {"current_date": [2016, 1, 1, 0, 0, 0], "hours": 12},
-            "fv_nwp_nudge_nml": {"file_names": [f"INPUT/{old_nudging_file}"]},
-        },
-        "patch_files": [old_asset],
+    test_config["gfs_analysis_data"] = {"url": url, "filename_pattern": pattern}
+    test_config["namelist"]["coupler_nml"]["current_date"] = [2016, 1, 1, 0, 0, 0]
+    test_config["namelist"]["coupler_nml"]["hours"] = 12
+    test_config["namelist"]["fv_nwp_nudge_nml"] = {
+        "file_names": [f"INPUT/{old_nudging_file}"]
     }
+    test_config["patch_files"] = [old_asset]
 
     fv3config.update_config_for_nudging(test_config)
 

tests/test_tables.py
@@ -1,4 +1,5 @@
 import unittest
+import copy
 import os
 import shutil
 from fv3config import ConfigError
@@ -6,6 +7,7 @@ from fv3config._tables import update_diag_table_for_config
 from fv3config.config.derive import (
     get_current_date,
     _get_current_date_from_coupler_res,
+    _get_coupler_res_filename,
 )
 from fv3config._datastore import (
     get_microphysics_name,
@@ -76,19 +78,19 @@ class TableTests(unittest.TestCase):
         return full_path
 
     def test_default_data_table_filename(self):
-        config = DEFAULT_CONFIG.copy()
+        config = copy.deepcopy(DEFAULT_CONFIG)
         filename = get_data_table_filename(config)
         self.assertTrue(os.path.isfile(filename))
         self.assertTrue("data_table" in filename)
 
     def test_default_diag_table_filename(self):
-        config = DEFAULT_CONFIG.copy()
+        config = copy.deepcopy(DEFAULT_CONFIG)
         filename = get_diag_table_filename(config)
         self.assertTrue(os.path.isfile(filename))
         self.assertTrue("diag_table" in filename)
 
     def test_default_field_table_filename(self):
-        config = DEFAULT_CONFIG.copy()
+        config = copy.deepcopy(DEFAULT_CONFIG)
         filename = get_field_table_filename(config)
         self.assertTrue(os.path.isfile(filename))
         self.assertTrue("field_table" in filename)
@@ -97,7 +99,7 @@ class TableTests(unittest.TestCase):
         source_rundir = self.make_run_directory("source_rundir")
         data_table_filename = os.path.join(source_rundir, "data_table")
         open(data_table_filename, "w").close()
-        config = DEFAULT_CONFIG.copy()
+        config = copy.deepcopy(DEFAULT_CONFIG)
         config["data_table"] = data_table_filename
         filename = get_data_table_filename(config)
         self.assertEqual(filename, data_table_filename)
@@ -106,26 +108,26 @@ class TableTests(unittest.TestCase):
         source_rundir = self.make_run_directory("source_rundir")
         diag_table_filename = os.path.join(source_rundir, "diag_table")
         open(diag_table_filename, "w").close()
-        config = DEFAULT_CONFIG.copy()
+        config = copy.deepcopy(DEFAULT_CONFIG)
         config["diag_table"] = diag_table_filename
         filename = get_diag_table_filename(config)
         self.assertEqual(filename, diag_table_filename)
 
     def test_get_bad_field_table_filename(self):
-        config = DEFAULT_CONFIG.copy()
+        config = copy.deepcopy(DEFAULT_CONFIG)
         config["namelist"]["gfs_physics_nml"]["imp_physics"] = -1
         with self.assertRaises(NotImplementedError):
             get_field_table_filename(config)
 
     def test_get_bad_microphysics_name_from_config(self):
-        config = DEFAULT_CONFIG.copy()
+        config = copy.deepcopy(DEFAULT_CONFIG)
         config["namelist"]["gfs_physics_nml"]["imp_physics"] = -1
         with self.assertRaises(NotImplementedError):
             get_microphysics_name(config)
 
     def test_get_bad_diag_table_filename(self):
         diag_table_filename = "/not/a/path/diag_table"
-        config = DEFAULT_CONFIG.copy()
+        config = copy.deepcopy(DEFAULT_CONFIG)
         config["diag_table"] = diag_table_filename
         with self.assertRaises(ConfigError):
             get_diag_table_filename(config)
@@ -161,9 +163,48 @@ class TableTests(unittest.TestCase):
         with self.assertRaises(ConfigError):
             _get_current_date_from_coupler_res(coupler_res_filename)
 
-    def test_get_current_date_from_config(self):
-        config = DEFAULT_CONFIG.copy()
+    def test_get_current_date_from_config_force_date_true(self):
+        config = copy.deepcopy(DEFAULT_CONFIG)
         config["namelist"]["coupler_nml"]["force_date_from_namelist"] = True
+        config["namelist"]["coupler_nml"]["current_date"] = valid_current_date
+        current_date = get_current_date(config)
+        self.assertEqual(current_date, valid_current_date)
+
+    def test_get_current_date_from_config_force_date_false(self):
+        config = copy.deepcopy(DEFAULT_CONFIG)
+        config["namelist"]["coupler_nml"]["force_date_from_namelist"] = False
+        config["namelist"]["coupler_nml"]["current_date"] = valid_current_date
+        current_date = get_current_date(config)
+        self.assertEqual(current_date, valid_current_date)
+
+    def test_get_current_date_from_config_which_includes_coupler_res_asset(self):
+        config = copy.deepcopy(DEFAULT_CONFIG)
+        tmpdir = self.make_run_directory("test_dir")
+        config["patch_files"] = {
+            "source_location": tmpdir,
+            "source_name": "coupler.res",
+            "target_location": "INPUT",
+            "target_name": "coupler.res",
+            "copy_method": "copy",
+        }
+        with open(os.path.join(tmpdir, "coupler.res"), "w") as f:
+            f.write(valid_coupler_res)
+        current_date = get_current_date(config)
+        self.assertEqual(current_date, valid_current_date)
+
+    def test_get_coupler_res_filename_from_bytes_coupler_res_asset(self):
+        config = copy.deepcopy(DEFAULT_CONFIG)
+        config["patch_files"] = {
+            "bytes": b"some data",
+            "target_location": "INPUT",
+            "target_name": "coupler.res",
+        }
+        with self.assertRaises(NotImplementedError):
+            _get_coupler_res_filename(config)
+
+    def test_get_current_date_from_config_empty_initial_conditions(self):
+        config = copy.deepcopy(DEFAULT_CONFIG)
+        config["initial_conditions"] = []
         config["namelist"]["coupler_nml"]["current_date"] = valid_current_date
         current_date = get_current_date(config)
         self.assertEqual(current_date, valid_current_date)